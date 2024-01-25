bc18ad12a3aef75e001c50f989fc1ea55885216d
abbassDaher <abbassDaher1@outlook.com>
2024-01-25 08:31:07 +0200

    use useRef with use effetct to get the previous term useRef save the value globale without rendering deffrence for setState

--- src/App.js
+++ src/App.js
@@ -1,70 +1,60 @@
-import React, { useState, useEffect, Fragment } from "react";
+import React, { useState, useEffect, Fragment, useRef } from "react";
 // import Form from "./components/App/Form/Form";
 import axios from "axios";
 
 function App() {
-  const [term, setTerm] = useState("");
+  const [term, setTerm] = useState("javascript");
   const [result, setresult] = useState([]);
-  // const [state, setstate] = useState("");
-  // const getTermHandler = (term) => {
-  //   if (term)
-  //     setstate((prevState) => {
-  //       return term;
-  //     });
-  // };
-  // ------------------------------------------------------------------------?
-  //  https://en.wikipedia.org/w/api.php?
-  // action=query&
-  // list=search&
-  // prop=info&
-  // inprop=url&
-  // utf8=&
-  // format=json&
-  // origin=*&
-  // srlimit=20&
-  // srsearch=java
+
+  const prevTermState = useRef();
 
   useEffect(() => {
-    const search = async () => {
-      const respond = await axios.get("https://en.wikipedia.org/w/api.php", {
-        params: {
-          action: "query",
-          list: "search",
-          prop: "info",
-          inprop: "url",
-          utf8: "",
-          format: "json",
-          origin: "*",
-          srlimit: 20,
-          srsearch: term,
-        },
-      });
-      setresult(respond.data.query.search);
-      // console.log();
-    };
-    /* The `debounceSearch` constant is using the `setTimeout` function to delay the execution of the
-    `search` function by 1000 milliseconds (1 second). It is used to implement a debounce
-    functionality, which means that the `search` function will only be called after the user has
-    stopped typing for 1 second.*/
-    const debounceSearch = setTimeout(() => {
-      if (term) {
-        search();
-      }
-    }, 1000);
-    return () => clearTimeout(debounceSearch);
-  }, [term]);
-
-  const fetchData = result.map((el) => {
-    return (
-      <tr key={el.pageid}>
-        <td scope="row">{el.pageid}</td>
-        <td scope="row">{el.title}</td>
-        <td scope="row">
-          <span dangerouslySetInnerHTML={{ __html: el.snippet }} />
-        </td>
-      </tr>
-    );
+    prevTermState.current = term;
   });
+  const prevTerm = prevTermState.current;
+
+  // useEffect(() => {
+  //   const search = async () => {
+  //     const respond = await axios.get("https://en.wikipedia.org/w/api.php", {
+  //       params: {
+  //         action: "query",
+  //         list: "search",
+  //         prop: "info",
+  //         inprop: "url",
+  //         utf8: "",
+  //         format: "json",
+  //         origin: "*",
+  //         srlimit: 20,
+  //         srsearch: term,
+  //       },
+  //     });
+  //     setresult(respond.data.query.search);
+  //     // console.log();
+  //   };
+
+  //   /* The `debounceSearch` constant is using the `setTimeout` function to delay the execution of the
+  //   `search` function by 1000 milliseconds (1 second). It is used to implement a debounce
+  //   functionality, which means that the `search` function will only be called after the user has
+  //   stopped typing for 1 second.*/
+  //   const debounceSearch = setTimeout(() => {
+  //     if (term) {
+  //       search();
+  //     }
+  //   }, 1000);
+  //   return () => clearTimeout(debounceSearch);
+  // }, [term]);
+
+  // const fetchData = result.map((el) => {
+  //   return (
+  //     <tr key={el.pageid}>
+  //       <td scope="row">{el.pageid}</td>
+  //       <td scope="row">{el.title}</td>
+  //       <td scope="row">
+  //         <span dangerouslySetInnerHTML={{ __html: el.snippet }} />
+  //       </td>
+  //     </tr>
+  //   );
+  // });
 
   return (
     <Fragment>
@@ -74,8 +64,10 @@ function App() {
         id="exampleInputEmail1"
         placeholder="search..."
         onChange={(e) => setTerm(e.target.value)}
-        value={term}
       />
+      <p>current Term: {term}</p>
+      <p>prev Term: {prevTerm}</p>
+
       <table className="table">
         <thead>
           <tr>
@@ -85,7 +77,6 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          <tr>{fetchData}</tr>
         </tbody>
       </table>
     </Fragment>
